5cb61174c19c9f41edbf6a38b150d17c576d1691
i6rahim <i6rahim210@gmail.com>
2022-11-19 16:00:20 +0300

    "add swiper"

--- js/main.js
+++ js/main.js
@@ -49,3 +49,44 @@ function headerNav() {
 
 }
 headerNav()
+
+
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+  slidesPerView: 3,
+  spaceBetween: 30,
+
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 20
+    },
+    // when window width is >= 480px
+    680: {
+      slidesPerView: 2,
+      spaceBetween: 25
+    },
+    // when window width is >= 640px
+    780: {
+      slidesPerView: 3,
+      spaceBetween: 30
+    }
+  }
+});
